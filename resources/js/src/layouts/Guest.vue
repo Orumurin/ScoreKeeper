4beaf173c83499032b68e656eaa0bb5479a8c625
orumurin <orumurin@gmail.com>
2022-08-30 12:17:29 +0300

    Add active link

--- resources/js/src/layouts/Guest.vue
+++ resources/js/src/layouts/Guest.vue
@@ -28,4 +28,8 @@
         height: 100vh;
         background: conic-gradient(from 98.68deg at 50% 50%, rgba(27, 149, 188, 0.21) 0deg, #6417E1 18.28deg, rgba(68, 77, 209, 0.662513) 186.95deg, rgba(27, 149, 188, 0.21) 360deg, rgba(27, 149, 188, 0.21) 360deg);
     }
+    .router-link-exact-active {
+        border-bottom: 2px solid #f5bf35;
+        padding-bottom: 8px;
+    }
 </style>
