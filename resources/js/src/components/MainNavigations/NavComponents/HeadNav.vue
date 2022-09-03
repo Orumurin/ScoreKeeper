5d2ffb41d2646d1df3cad89a94f0022b4f54560f
orumurin <orumurin@gmail.com>
2022-09-03 22:02:44 +0300

    Fix header nav layout

--- resources/js/src/components/MainNavigations/NavComponents/HeadNav.vue
+++ resources/js/src/components/MainNavigations/NavComponents/HeadNav.vue
@@ -1,14 +1,17 @@
 <template>
-    <div class="flex flex-col text-center items-center">
-        <RouterLink to="/profile">
+    <div class="flex flex-col items-center">
+        <RouterLink 
+            to="/profile" 
+            class="flex flex-col text-center items-center justify-center"
+        >
             <img
                 class="w-12"
                 src="@/assets/images/icons/person.svg"
                 alt="person"
             />
             <span class="text-2xl text-gray-700 pt-4 pb-1">
-            {{ userState.user.name }}
-        </span>
+                {{ userState.user.name }}
+            </span>
         </RouterLink>
         <div class="h-px w-24 bg-gray-500"></div>
     </div>
